a432e3d5724fd001548827d905cd50e834734078
Jang Jae Won <jaewjang@ebay.com>
2017-10-17 17:23:46 +0900

    gulpfile task 수정

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,33 +1,32 @@
 const gulp = require('gulp');
-const clean = require('gulp-clean');
-// const filter = require('gulp-filter');
-
+const del = require('del');
+const watch = require('gulp-watch');
 const include = require('gulp-replace-include');
 const sass = require('gulp-ruby-sass');
 const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('clean', () =>
-    gulp.src('build')
-    .pipe(clean({force: true}))
+    del(['build']).then(paths => {
+        console.log('Delete Target:\n', paths.join('\n'));
+    })
 );
 
 gulp.task('html', function(){
   return gulp.src('src/html/**/*.html')
     .pipe(include({
-        src: 'src/html/app/'
+        src: 'src/html/app'
         ,include: ['src/html/include/']
         ,prefix: '@@'
-        // ,global: {
-        //     'smilepay': '장재원'
-        // }
+        ,global: {
+            'smilepay': '장재원'
+        }
     }))
-    // .pipe(filter(['!src/html/include/']))
     .pipe(gulp.dest('build/html'))
 });
 
 gulp.task('sass', () =>
     sass('src/scss/**/*.scss',{
-        // sourcemap: true
+        sourcemap: true
     })
     .on('error', sass.logError)
     .pipe(sourcemaps.write('maps', {
@@ -41,3 +40,14 @@ gulp.task('build', ['html', 'sass']);
 gulp.task('default', ['clean'], () =>
     gulp.start('build')
 );
+/**
+ * TODO
+ * watch, merge, uglify, minify, html/scss/js lint, image sprite task
+ */
+
+ //   .pipe(watch('src/html/**/*.html'))
+ // .pipe(watch('src/scss/**/*.scss'))
+ // rm -rf node_modules
+// rm package-lock.json yarn.lock
+// npm cache clear --force
+// npm install
