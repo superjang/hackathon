a3cb8975f16355f2691d6ec81eda68a241aacac8
장재원 <cresumerjang@gmail.com>
2017-10-18 01:05:06 +0900

    gul task 추가

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const del = require('del');
 const watch = require('gulp-watch');
+const batch = require('gulp-batch');
 const include = require('gulp-replace-include');
 const sass = require('gulp-ruby-sass');
 const sourcemaps = require('gulp-sourcemaps');
@@ -12,37 +13,46 @@ gulp.task('clean', () =>
 );
 
 gulp.task('html', function(){
-  return gulp.src('src/html/**/*.html')
+  return gulp.src('src\/html\/**\/*.html')
     .pipe(include({
-        src: 'src/html/app'
-        ,include: ['src/html/include/']
+        src: 'src\/html\/app'
+        ,include: ['src\/html\/include\/']
         ,prefix: '@@'
         ,global: {
             'smilepay': '장재원'
         }
     }))
-    .pipe(gulp.dest('build/html'))
+    .pipe(gulp.dest('build\/html'))
 });
 
 gulp.task('sass', () =>
-    sass('src/scss/**/*.scss',{
+    sass('src\/scss\/**\/*.scss',{
         sourcemap: true
     })
+    // .pipe('src\/scss\/**\/*.scss')
     .on('error', sass.logError)
     .pipe(sourcemaps.write('maps', {
         includeContent: false,
-        sourceRoot: 'src/scss'
+        sourceRoot: 'src\/scss'
     }))
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('build\/css'))
 );
 
-gulp.task('build', ['html', 'sass']);
-gulp.task('default', ['clean'], () =>
+gulp.task('watch', function () {
+    watch(['src\/html\/**\/*.html', 'src\/scss\/**\/*.scss', ], batch(function (events, done) {
+        gulp.start('build', done);
+    }));
+});
+
+gulp.task('build', ['html', 'sass', 'watch']);
+gulp.task('start', ['clean'], function(){
     gulp.start('build')
-);
+});
+
 /**
  * TODO
  * watch, merge, uglify, minify, html/scss/js lint, image sprite task
+ * gulp-copy
  */
 
  //   .pipe(watch('src/html/**/*.html'))
