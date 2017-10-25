d2281c97ee9a1471c4c7cd73fa7d702a9b6a2048
hyojeong <kimhjworld@hanmail.net>
2017-10-25 18:47:55 +0900

    스마일링커 기본 요구사항 정의 및 컴포넌트 리스트업

--- app/src/js/app/smilepay/visit/visit_vip.js
+++ app/src/js/app/smilepay/visit/visit_vip.js
@@ -1,10 +1,15 @@
 /* main category */
 function iframeBtnControl() {
-    //var $buyBtn = $('#smileLinker_btn_control');
-    var $buyBtn = $('.button--normal.button--buy');
-    $buyBtn.on("click", function() {
+    var $buyBtn = $('#smileLinker_btn_control'),
+         $iframeTarget = $('#vip_frame') ;
+
+     $buyBtn.on("click", function() {
        console.log("구매하기버튼 클릭");
+       $iframeTarget.attr('src', 'http://localhost:8889/html/app/smilepay/page__visitVIP_buyfinish.html');
     })
+
+
+
 
 }
 
