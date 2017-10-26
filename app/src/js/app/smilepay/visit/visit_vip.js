ffe9d6d76e80cbb4bb4a7db5086d8ad9725d3e70
장재원 <cresumerjang@gmail.com>
2017-10-26 11:24:53 +0900

    ASdf

--- app/src/js/app/smilepay/visit/visit_vip.js
+++ app/src/js/app/smilepay/visit/visit_vip.js
@@ -5,7 +5,7 @@ function iframeBtnControl() {
 
      $buyBtn.on("click", function() {
        console.log("구매하기버튼 클릭");
-       $iframeTarget.attr('src', 'http://localhost:8889/html/app/smilepay/page__visitVIP_buyfinish.html');
+       $iframeTarget.attr('src', '/html/app/smilepay/page__visitVIP_buyfinish.html');
     })
 
 
