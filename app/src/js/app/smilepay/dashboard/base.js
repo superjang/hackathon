4be4464b0c3a13ae7c8d080bb0f2886abcfa5442
hyojeong <kimhjworld@hanmail.net>
2017-10-26 13:48:29 +0900

    스마일링커 기본 요구사항 정의 및 컴포넌트 리스트업

--- app/src/js/app/smilepay/dashboard/base.js
+++ app/src/js/app/smilepay/dashboard/base.js
@@ -1,5 +1,5 @@
 var app = "dashboard";
-var linkerCount = storage.getData('userInfo').liveLinkerCount;
+var linkerCount =   storage.getData('userInfo').liveLinkerCount;
 
 
 var xline = ['오늘','2','3','4','5','6','7','8','9','10','11'];
